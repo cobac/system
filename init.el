4afe234f618fbb4f0453fba174cf5331b5e3acaa
Coba <coba@cobac.eu>
2020-10-13 16:24:33 +0200

    Removed speeddating

--- init.el
+++ init.el
@@ -286,33 +286,15 @@
   )
 
 ;; Numbers stuff
-(use-package speeddating
-  :straight t
-  )
 (use-package evil-numbers
   :straight t
   :after speeddating
-  :config
-  (defun coba-increase-at-pt nil
-    "Increment number or date (speeddating) at point.
-From Issue 14 of evil-numbers"
-    (interactive)
-    (condition-case nil
-        (speeddating-increase 1)
-      (error (evil-numbers/inc-at-pt 1))))
-  (defun coba-decrease-at-pt nil
-    "Decrease number or date (speeddating) at point
-From Issue 14 of evil-numbers"
-    (interactive)
-    (condition-case nil
-        (speeddating-decrease 1)
-      (error (evil-numbers/dec-at-pt 1))))
   :general
-  (general-def :states '(normal motion)
-    "C-a" 'coba-increase-at-pt
-    "C-x" 'coba-decrease-at-pt)
+  (general-def :states '(normal motion visual )
+    "C-a" 'evil-numbers/inc-at-pt
+    "C-x" 'evil-numbers/dec-at-pt
+    )
   )
-
 
 ;; Org
 

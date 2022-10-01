f9bd7350f7f6a2b02b8bdeb39ad13901cff9ff0b
Coba <coba@cobac.eu>
2022-10-01 22:00:39 +0200

    rust stuff

--- init.el
+++ init.el
@@ -1803,3 +1803,31 @@
     "K" 'image-previous-file
     )
   )
+
+;; Rust
+(use-package rustic
+  :straight t
+  :bind (:map rustic-mode-map
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t)
+  (add-hook 'rustic-mode-hook 'coba-rustic-autosave-mode-hook)
+
+  (defun coba-rustic-autosave-mode-hook ()
+    ;; so that run C-c C-c C-r works without having to confirm, but don't try to
+    ;; save rust buffers that are not file visiting. Once
+    ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
+    ;; no longer be necessary.
+    (when buffer-file-name
+      (setq-local buffer-save-without-query t))
+    (add-hook 'before-save-hook 'lsp-format-buffer nil t))
+
+  (evil-set-initial-state 'rustic-popup-mode 'emacs)
+  )
+

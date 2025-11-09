58b14cd1bd6601b1ddc16f733ec9c88671954e03
David Coba <coba@cobac.eu>
2025-11-09 22:05:49 +0100

    emacs: format big_init

--- emacs/files/big_init.el
+++ emacs/files/big_init.el
@@ -1,30 +1,32 @@
 ;; mu4e
-(use-package
-  mu4e
+(use-package mu4e
   :straight t
   :general (coba-leader-def "m" 'mu4e)
   :config (add-hook 'mu4e-compose-mode-hook 'flyspell-mode)
-  (setq
-   mail-user-agent 'mu4e-user-agent
-   mu4e-change-filenames-when-moving t
-   mu4e-get-mail-command "/usr/bin/mbsync -a"
-   mu4e-update-interval 120
-   ;;mu4e-html2text-command "html2text --unicode-snob"
-   ;;shr-color-visible-luminance-min 80
-   message-kill-buffer-on-exit t
-   mu4e-index-update-error-warning nil
-   mu4e-view-show-images t
-   mu4e-attachment-dir "/home/coba/Downloads"
-   message-send-mail-function 'smtpmail-send-it
-   mu4e-compose-dont-reply-to-self t
-   mu4e-context-policy 'pick-first
-   mu4e-compose-context-policy 'ask)
+  (setq mail-user-agent 'mu4e-user-agent
+        mu4e-change-filenames-when-moving t
+        mu4e-get-mail-command "/usr/bin/mbsync -a"
+        mu4e-update-interval 120
+        ;;mu4e-html2text-command "html2text --unicode-snob"
+        ;;shr-color-visible-luminance-min 80
+        message-kill-buffer-on-exit t
+        mu4e-index-update-error-warning nil
+        mu4e-view-show-images t
+        mu4e-attachment-dir "/home/coba/Downloads"
+        message-send-mail-function 'smtpmail-send-it
+        mu4e-compose-dont-reply-to-self t
+        mu4e-context-policy 'pick-first
+        mu4e-compose-context-policy 'ask)
   (add-to-list
    'mu4e-bookmarks
-   '(:name "Coba Inbox" :query "maildir:/coba/Inbox" :key ?j))
+   '(:name "Coba Inbox"
+           :query "maildir:/coba/Inbox"
+           :key ?j))
   (add-to-list
    'mu4e-bookmarks
-   '(:name "Cosas Inbox" :query "maildir:/cosas/Inbox" :key ?k))
+   '(:name "Cosas Inbox"
+           :query "maildir:/cosas/Inbox"
+           :key ?k))
   (setq mu4e-contexts
         (list
          (make-mu4e-context
@@ -63,15 +65,12 @@
             (smtpmail-smtp-server . "mail.your-server.de")
             (smtpmail-stream-type . ssl)
             (smtpmail-smtp-service . 465)))))
-
-  (setq
-   mu4e-split-view 'horizontal
-   mu4e-view-show-addresses t
-   mu4e-view-show-images t)
+  (setq mu4e-split-view 'horizontal
+        mu4e-view-show-addresses t
+        mu4e-view-show-images t)
   (add-to-list
    'mu4e-view-actions '("browser" . mu4e-action-view-in-browser)
    t)
-
   (coba-local-leader-def
     'mu4e-view-mode-map "t"
     '(lambda ()
@@ -83,7 +82,8 @@
    '(archive
      :char "A"
      :prompt "Archive"
-     :dyn-target (lambda (target msg) (mu4e-get-refile-folder msg))
+     :dyn-target (lambda (target msg)
+                   (mu4e-get-refile-folder msg))
      :show-target (lambda (target) "archive")
      :action
      (lambda (docid msg target)
@@ -96,14 +96,13 @@
     '(motion visual normal emacs)
     "A"
     'mu4e-headers-mark-for-archive)
-  (require 'mu4e-icalendar) (mu4e-icalendar-setup)
-  (setq
-   gnus-icalendar-org-capture-file "~/Sync/Org/todo.org"
-   gnus-icalendar-org-capture-headline '("Other"))
+  (require 'mu4e-icalendar)
+  (mu4e-icalendar-setup)
+  (setq gnus-icalendar-org-capture-file "~/Sync/Org/todo.org"
+        gnus-icalendar-org-capture-headline '("Other"))
   (gnus-icalendar-org-setup))
 
-(use-package
-  mu4e-alert
+(use-package mu4e-alert
   :straight t
   :config
   (mu4e-alert-set-default-style 'libnotify)
@@ -189,35 +188,27 @@
 (defun coba-define-org-tex-template ()
   "Define `org-latex-classes' concatenating snippets."
   (interactive)
-
   (setq org-tex-apa-template
         (coba-file-content-as-string
          "~/.emacs.d/ox-templates/apa-article.tex"))
-
   (setq org-tex-apa-es-template
         (coba-file-content-as-string
          "~/.emacs.d/ox-templates/apa-article-es.tex"))
-
   (setq org-tex-report-template
         (coba-file-content-as-string
          "~/.emacs.d/ox-templates/report.tex"))
-
   (setq org-tex-graphix-template
         (coba-file-content-as-string
          "~/.emacs.d/ox-templates/graphix.tex"))
-
   (setq org-tex-footmisc-template
         (coba-file-content-as-string
          "~/.emacs.d/ox-templates/footmisc.tex"))
-
   (setq org-tex-math-template
         (coba-file-content-as-string
          "~/.emacs.d/ox-templates/math.tex"))
-
   (setq org-tex-listings-template
         (coba-file-content-as-string
          "~/.emacs.d/ox-templates/listings.tex"))
-
   (setq org-latex-classes
         `(("custom"
            ,(format "%s
@@ -230,7 +221,6 @@
            ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
            ("\\paragraph{%s}" . "\\paragraph*{%s}")
            ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
-
           ("custom-apa"
            ,(format
              "%s
@@ -243,7 +233,6 @@
            ("\\subsubsection*{%s}" . "\\subsubsection*{%s}")
            ("\\paragraph*{%s}" . "\\paragraph*{%s}")
            ("\\subparagraph*{%s}" . "\\subparagraph*{%s}"))
-
           ("article"
            "\\documentclass[11pt]{article}
 [NO-DEFAULT-PACKAGES]
@@ -256,6 +245,7 @@
            ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))))
 
 (coba-define-org-tex-template)
+
 ;; TODO: Check what's up with codeblocks
 
 (defun coba-org-latex-remove-title (str)
@@ -276,55 +266,54 @@
 (advice-add
  'org-latex-template
  :filter-return 'coba-org-latex-remove-title)
+
 (advice-add
  'org-latex-template
  :filter-return 'coba-org-latex-remove-author)
+
 (advice-add
  'org-latex-template
  :filter-return 'coba-org-latex-remove-date)
 
-(setq
- org-startup-with-inline-images t
- org-latex-compiler "xelatex"
- org-latex-bib-compiler "bibtex"
+(setq org-startup-with-inline-images t
+      org-latex-compiler "xelatex"
+      org-latex-bib-compiler "bibtex"
                                         ;org-export-in-background t
- org-latex-default-class "custom"
- org-latex-hyperref-template nil
- org-latex-prefer-user-labels t
- org-export-with-toc nil
- org-latex-src-block-backend 'engraved
- org-latex-listings-options
- '(("basicstyle" "\\ttfamily\\color{code-fg}")
-   ("stringstyle" "\\ttfamily\\color{code-string}")
-   ("commentstyle" "\\color{code-comment}")
-   ("keywordstyle" "\\color{code-keyword}")
-   ("upquote" "true")
-   ("backgroundcolor" "\\color{code-bg}")
-   ("frame" "single")
-   ("framesep" "5pt")
-   ("rulecolor" "\\color{code-comment}")
-   ("framerule" "1pt"))
- org-latex-pdf-process
- '("ln -s ~/Sync/Brain/bib.bib bib.bib"
-   "latexmk -pdflatex='xelatex -interaction nonstopmode' -shell-escape -pdf -bibtex -f %f"))
+      org-latex-default-class "custom"
+      org-latex-hyperref-template nil
+      org-latex-prefer-user-labels t
+      org-export-with-toc nil
+      org-latex-src-block-backend 'engraved
+      org-latex-listings-options
+      '(("basicstyle" "\\ttfamily\\color{code-fg}")
+        ("stringstyle" "\\ttfamily\\color{code-string}")
+        ("commentstyle" "\\color{code-comment}")
+        ("keywordstyle" "\\color{code-keyword}")
+        ("upquote" "true")
+        ("backgroundcolor" "\\color{code-bg}")
+        ("frame" "single")
+        ("framesep" "5pt")
+        ("rulecolor" "\\color{code-comment}")
+        ("framerule" "1pt"))
+      org-latex-pdf-process
+      '("ln -s ~/Sync/Brain/bib.bib bib.bib"
+        "latexmk -pdflatex='xelatex -interaction nonstopmode' -shell-escape -pdf -bibtex -f %f"))
 
 (add-to-list 'org-latex-default-packages-alist '("" "txfonts" t))
+
 (add-to-list 'org-latex-default-packages-alist '("" "graphviz" t))
 
 ;; Latex
-(use-package
-  tex
+(use-package tex
   :straight auctex
   :config (TeX-global-PDF-mode t)
-  (setq
-   TeX-source-correlate-mode t
-   TeX-source-correlate-start-server t
-   TeX-parse-self t
-   TeX-auto-save t
-   TeX-view-program-selection '((output-pdf "Zathura"))
-   LaTeX-command "xelatex --synctex=1"
-   pdf-latex-command "XeLaTeX")
-
+  (setq TeX-source-correlate-mode t
+        TeX-source-correlate-start-server t
+        TeX-parse-self t
+        TeX-auto-save t
+        TeX-view-program-selection '((output-pdf "Zathura"))
+        LaTeX-command "xelatex --synctex=1"
+        pdf-latex-command "XeLaTeX")
   (add-hook
    'LaTeX-mode-hook
    (lambda ()
@@ -335,15 +324,13 @@
         TeX-run-TeX
         nil
         t))
-     (setq
-      TeX-command-extra-options "-file-line-error -shell-escape"
-      TeX-command-default "XeLaTeX"
-      TeX-auto-untabify t
-      TeX-engine 'xetex
-      TeX-show-compilation nil)
+     (setq TeX-command-extra-options "-file-line-error -shell-escape"
+           TeX-command-default "XeLaTeX"
+           TeX-auto-untabify t
+           TeX-engine 'xetex
+           TeX-show-compilation nil)
      (TeX-global-PDF-mode t)
      (setq TeX-save-query nil)))
-
   (coba-local-leader-def
     :keymaps
     'LaTeX-mode-map
@@ -367,13 +354,11 @@
     'TeX-command-run-all
     "C-c C-a"
     'TeX-command-master)
-
   (defun coba-texcount ()
     "Print number of output words from the current buffer. It invoques texcount."
     (interactive)
     (save-buffer)
     (shell-command (concat "texcount " (buffer-name))))
-
   (defun coba-bibtex-check ()
     "Check the validity of a bibtex file."
     (interactive)
@@ -383,23 +368,20 @@
     :keymaps 'bibtex-mode-map "c" 'coba-bibtex-check))
 
 ;; ESS
-(use-package
-  ess
+(use-package ess
   :straight t
   ;; :hook
   ;; (ess-r-mode . lsp)
   ;; (inferior-ess-r-mode . lsp)
   :config
   (require 'ess-site)
-  (setq
-   ess-ask-for-ess-directory nil
-   comint-move-point-for-output 'others
-   comint-scroll-to-bottom-on-input 'this
-   ess-eval-visibly 'nowait
-   ess-style 'RStudio)
+  (setq ess-ask-for-ess-directory nil
+        comint-move-point-for-output 'others
+        comint-scroll-to-bottom-on-input 'this
+        ess-eval-visibly 'nowait
+        ess-style 'RStudio)
   (evil-set-initial-state 'ess-r-help-mode 'motion)
   (general-def 'ess-r-help-mode-map "q" 'quit-window)
-
   (general-def
     :keymaps '(comint-mode-map)
     :states
@@ -412,75 +394,74 @@
     'comint-previous-matching-input-from-input
     "C-ñ"
     'ess-insert-assign)
-
   (general-def
     :states '(insert)
     :keymaps
     'ess-mode-map
     "C-ñ"
     'ess-insert-assign)
-
   (coba-local-leader-def :keymaps 'ess-mode-map "b" 'ess-eval-buffer))
 
 (evil-set-initial-state 'ess-julia-help-mode 'motion)
+
 (general-def 'ess-julia-help-mode-map "q" 'quit-window)
 
-(use-package poly-R :straight t)
+(use-package poly-R
+  :straight t)
 
-(use-package
-  poly-markdown
+(use-package poly-markdown
   :straight t
   :config
   (add-to-list 'auto-mode-alist '("\\.Rmd" . poly-markdown+r-mode))
   (add-to-list 'auto-mode-alist '("\\.md" . poly-markdown+r-mode)))
 
 ;;  Stan
-(use-package
-  stan-mode
+(use-package stan-mode
   :straight t
   :after company
   :mode ("\\.stan\\'" . stan-mode)
   :hook (stan-mode . stan-mode-setup)
   :config (setq stan-indentation-offset 2))
-(use-package
-  company-stan
+
+(use-package company-stan
   :straight t
   :after stan-mode
   :hook (stan-mode . company-stan-setup))
-(use-package
-  eldoc-stan
+
+(use-package eldoc-stan
   :straight t
   :after stan-mode
   :hook (stan-mode . eldoc-stan-setup))
-(use-package
-  flycheck-stan
+
+(use-package flycheck-stan
   :straight t
   :after stan-mode
   :hook
   ((stan-mode . flycheck-stan-stanc2-setup)
    (stan-mode . flycheck-stan-stanc3-setup))
   :config
-  (setq
-   flycheck-stanc-executable nil
-   flycheck-stanc3-executable nil))
-(use-package stan-snippets :straight t :after stan-mode)
+  (setq flycheck-stanc-executable nil
+        flycheck-stanc3-executable nil))
+
+(use-package stan-snippets
+  :straight t
+  :after stan-mode)
 
 ;; Julia
-(use-package
-  lsp-julia
-  :straight (:type git :host github :repo "non-Jedi/lsp-julia")
+(use-package lsp-julia
+  :straight (:type git
+                   :host github
+                   :repo "non-Jedi/lsp-julia")
   :after lsp-mode
   :config
   (setq lsp-julia-default-environment "~/.julia/environments/v1.6")
   (setq lsp-julia-timeout 99999999))
 
-(use-package
-  julia-mode
+(use-package julia-mode
   :straight t
   :config (add-hook 'julia-mode-hook #'lsp))
 
-(use-package
-  julia-vterm
+(use-package julia-vterm
   :straight t
   :hook (julia-mode . julia-vterm-mode)
   :config
@@ -494,7 +475,6 @@
     'julia-vterm-send-current-line
     "C-t"
     'coba-julia-vterm-define-threads)
-
   (defun coba-julia-vterm-define-threads ()
     (interactive)
     (let ((val (read-from-minibuffer "Number of threads: ")))
@@ -502,8 +482,7 @@
             (concat "/usr/bin/julia -t " val))))
   (setq display-buffer-reuse-frames t))
 
-(use-package
-  ob-julia-vterm
+(use-package ob-julia-vterm
   :straight t
   :config (add-to-list 'org-babel-load-languages '(julia-vterm . t))
   (org-babel-do-load-languages
@@ -523,11 +502,9 @@
 ;;   )
 
 ;; Haskell
-(use-package
-  haskell-mode
+(use-package haskell-mode
   :straight t
   :general
-
   (general-def
     '(haskell-mode-map haskell-interactive-mode-map) "C-ñ"
     '(lambda ()
@@ -544,7 +521,8 @@
        (save-buffer)
        (haskell-process-load-file)))
   (coba-local-leader-def
-    'haskell-mode-map "r" 'hlint-refactor-refactor-at-point "h" 'hoogle)
+    'haskell-mode-map "r" 'hlint-refactor-refactor-at-point "h"
+    'hoogle)
   (general-def
     'haskell-interactive-mode-map
     :states
@@ -566,18 +544,17 @@
 ;;(setq ormolu-no-cabal t)
 ;;)
 
-(use-package
-  flycheck-haskell
+(use-package flycheck-haskell
   :straight t
   :config (add-hook 'haskell-mode-hook #'flycheck-haskell-setup)
   (setq-default flycheck-disabled-checkers
                 '(haskell-ghc haskell-stack-ghc)))
 
-(use-package hlint-refactor :straight t)
+(use-package hlint-refactor
+  :straight t)
 
 ;; nov
-(use-package
-  nov
+(use-package nov
   :straight t
   :config (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
   (defun coba-nov-font-setup ()
@@ -588,27 +565,22 @@
   (add-hook 'nov-mode-hook 'coba-nov-font-setup))
 
 ;; Rust
-(use-package
-  rustic
+(use-package rustic
   :straight t
   :config
   ;; uncomment for less flashiness
   ;; (setq lsp-eldoc-hook nil)
   ;; (setq lsp-enable-symbol-highlighting nil)
   ;; (setq lsp-signature-auto-activate nil)
-
   ;; comment to disable rustfmt on save
   (setq rustic-format-on-save t)
   (add-hook 'before-save-hook 'lsp-format-buffer nil t)
-
   (defun coba-rustic-autosave-mode-hook ()
     "Enable auto-saving in rustic-mode buffers."
     (when buffer-file-name
       (setq-local compilation-ask-about-save nil)))
   (add-hook 'rustic-mode-hook 'coba-rustic-autosave-mode-hook)
-
   (evil-set-initial-state 'rustic-popup-mode 'emacs)
-
   (general-def
     'rustic-mode-map
     "C-c C-c s"
@@ -624,7 +596,6 @@
     '(lambda ()
        (interactive)
        (insert "-> ")))
-
   (defun coba-rustic-cargo-doc-std ()
     "Open the documentation for the standard library in a browser."
     (interactive)
@@ -634,13 +605,12 @@
     (interactive "scargo new: ")
     (message (format "cargo new %s" args))
     (shell-command (format "cargo new %s" args)))
-
   (add-hook
    'rustic-mode-hook
-   (lambda () (push '(?< . ("< " . " >")) evil-surround-pairs-alist))))
+   (lambda ()
+     (push '(?< . ("< " . " >")) evil-surround-pairs-alist))))
 
-(use-package
-  ledger-mode
+(use-package ledger-mode
   :straight t
   :config
   (defun coba-hledger-call (command)
@@ -656,7 +626,6 @@
     (windmove-right)
     (delete-other-windows)
     (text-scale-decrease 2))
-
   (coba-leader-def
     "eb"
     '(lambda ()
@@ -667,8 +636,7 @@
        (interactive)
        (coba-hledger-call "is"))))
 
-(use-package
-  gleam-ts-mode
+(use-package gleam-ts-mode
   :straight t
   :mode (rx ".gleam" eos)
   :config
@@ -685,7 +653,7 @@
                '(gleam-ts-mode . ("gleam" "lsp")))
   (add-hook 'gleam-ts-mode-hook 'eglot-ensure))
 
-(use-package exercism :straight t)
+;; (use-package exercism :straight t)
 
 (use-package vue-mode
   :straight t

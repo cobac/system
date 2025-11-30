5e81e46f0f3e53fc6210f8a22a5f6a38668e0518
David Coba <coba@cobac.eu>
2025-11-30 23:34:14 +0100

    emacs: enable emark

--- emacs/files/init.el
+++ emacs/files/init.el
@@ -366,63 +366,63 @@
   :straight t
   :config (marginalia-mode))
 
-;; (use-package embark
-;;   :straight t
-;;   :after consult
-;;   :bind (("C-." . embark-act)
-;;          ("C-)" . embark-dwim))
-;;   :config
-;;   ;; C-d: kill-buffer during switch-to-buffer
-;;   ;; https://www.reddit.com/r/emacs/comments/16g08me/killbuffer_from_the_minibuffer_after_mx/
-;;   ;; no confirmation
-;;   (setf (alist-get 'kill-buffer embark-pre-action-hooks)
-;;         ())
-;;   ;; ;; don't embark--restart
-;;   (setf (alist-get 'kill-buffer embark-post-action-hooks)
-;;         ())
-;;   ;; ;; don't quit
-;;   (setq embark-quit-after-action '((kill-buffer . nil) (t . t)))
-;;   (general-def
-;;     :keymaps 'minibuffer-local-map
-;;     :states '(insert normal)
-;;     "C-d" (general-simulate-key "C-. k C-j"))
-;;   ;; https://github.com/oantolin/embark/wiki/Additional-Configuration#use-which-key-like-a-key-menu-prompt
-;;   (defun embark-which-key-indicator ()
-;;     "An embark indicator that displays keymaps using which-key.
-;; The which-key help message will show the type and value of the
-;; current target followed by an ellipsis if there are further
-;; targets."
-;;     (lambda (&optional keymap targets prefix)
-;;       (if (null keymap)
-;;           (which-key--hide-popup-ignore-command)
-;;         (which-key--show-keymap
-;;          (if (eq (plist-get (car targets) :type) 'embark-become)
-;;              "Become"
-;;            (format "Act on %s '%s'%s"
-;;                    (plist-get (car targets) :type)
-;;                    (embark--truncate-target (plist-get (car targets)
-;;                                                        :target))
-;;                    (if (cdr targets) "…" "")))
-;;          (if prefix
-;;              (pcase (lookup-key keymap prefix 'accept-default)
-;;                ((and (pred keymapp) km) km)
-;;                (_ (key-binding prefix 'accept-default)))
-;;            keymap)
-;;          nil nil t
-;;          (lambda (binding)
-;;            (not (string-suffix-p "-argument" (cdr binding))))))))
-;;   (setq embark-indicators
-;;         '(embark-which-key-indicator
-;;           embark-highlight-indicator
-;;           embark-isearch-highlight-indicator))
-;;   (defun embark-hide-which-key-indicator (fn &rest args)
-;;     "Hide the which-key indicator immediately when using the completing-read prompter."
-;;     (which-key--hide-popup-ignore-command)
-;;     (let ((embark-indicators
-;;            (remq #'embark-which-key-indicator embark-indicators)))
-;;       (apply fn args)))
-;;   (advice-add #'embark-completing-read-prompter
-;;               :around #'embark-hide-which-key-indicator))
+(use-package embark
+  :straight t
+  :after consult
+  :bind (("C-." . embark-act)
+         ("C-)" . embark-dwim))
+  :config
+  ;; C-d: kill-buffer during switch-to-buffer
+  ;; https://www.reddit.com/r/emacs/comments/16g08me/killbuffer_from_the_minibuffer_after_mx/
+  ;; no confirmation
+  (setf (alist-get 'kill-buffer embark-pre-action-hooks)
+        ())
+  ;; ;; don't embark--restart
+  (setf (alist-get 'kill-buffer embark-post-action-hooks)
+        ())
+  ;; ;; don't quit
+  (setq embark-quit-after-action '((kill-buffer . nil) (t . t)))
+  (general-def
+    :keymaps 'minibuffer-local-map
+    :states '(insert normal)
+    "C-d" (general-simulate-key "C-. k C-j"))
+  ;; https://github.com/oantolin/embark/wiki/Additional-Configuration#use-which-key-like-a-key-menu-prompt
+  (defun embark-which-key-indicator ()
+    "An embark indicator that displays keymaps using which-key.
+The which-key help message will show the type and value of the
+current target followed by an ellipsis if there are further
+targets."
+    (lambda (&optional keymap targets prefix)
+      (if (null keymap)
+          (which-key--hide-popup-ignore-command)
+        (which-key--show-keymap
+         (if (eq (plist-get (car targets) :type) 'embark-become)
+             "Become"
+           (format "Act on %s '%s'%s"
+                   (plist-get (car targets) :type)
+                   (embark--truncate-target (plist-get (car targets)
+                                                       :target))
+                   (if (cdr targets) "…" "")))
+         (if prefix
+             (pcase (lookup-key keymap prefix 'accept-default)
+               ((and (pred keymapp) km) km)
+               (_ (key-binding prefix 'accept-default)))
+           keymap)
+         nil nil t
+         (lambda (binding)
+           (not (string-suffix-p "-argument" (cdr binding))))))))
+  (setq embark-indicators
+        '(embark-which-key-indicator
+          embark-highlight-indicator
+          embark-isearch-highlight-indicator))
+  (defun embark-hide-which-key-indicator (fn &rest args)
+    "Hide the which-key indicator immediately when using the completing-read prompter."
+    (which-key--hide-popup-ignore-command)
+    (let ((embark-indicators
+           (remq #'embark-which-key-indicator embark-indicators)))
+      (apply fn args)))
+  (advice-add #'embark-completing-read-prompter
+              :around #'embark-hide-which-key-indicator))
 
 (use-package embark-consult
   :straight t
